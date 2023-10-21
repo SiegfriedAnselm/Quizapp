2091091846f7c0854d10f8a04eef2d70110cb167
SiegfriedAnselm <146078588+SiegfriedAnselm@users.noreply.github.com>
2023-10-21 14:35:25 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,169 @@
+let Schöpfung = [
+    {
+        "question": "Was ist HTML?",
+        "answer_1": "Softwareprogramm",
+        "answer_2": "Spiel für Erwachsene",
+        "answer_3": "Abkürzung für eine Partei",
+        "answer_4": "Bloß ein Schreibfehler",
+        "correct_answer": 1,
+    },
+    {
+        "question": "Wozu wird HTML benötigt?",
+        "answer_1": "Programmieren was man sieht",
+        "answer_2": "Programmieren wie man etwas sieht",
+        "answer_3": "Programmieren von Logic",
+        "answer_4": "Wie schon gesagt: Bloß ein Schreibfehler",
+        "correct_answer": 1,
+    },
+    {
+        "question":"Wann wurde HTML erstmals benutzt?",
+        "answer_1":"1999",
+        "answer_2":"1888",
+        "answer_3":"1789",
+        "answer_4":"1627",
+        "correct_answer":2,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    }
+];
+
+let zehnGebote = [
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    }
+];
+let Weihnachten = [
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    }
+];
+let Ostern = [
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    },
+    {
+        "question":"",
+        "answer_1":"",
+        "answer_2":"",
+        "answer_3":"",
+        "answer_4":"",
+        "correct_answer":1,
+    }
+];
---
+++ script.js
@@ -0,0 +1,49 @@
+let currentQuestion = 0;
+
+function render() {
+    document.getElementById('sumQuestion').innerHTML = Schöpfung.length;
+    showQuestion()
+}
+
+function showQuestion() {
+    let question = Schöpfung[currentQuestion];
+
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('activeQuestion').innerHTML = currentQuestion+1;
+}
+
+function answer(selection) {
+    let question = Schöpfung[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['correct_answer']}`
+
+    if(selectedQuestionNumber == question['correct_answer']){
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+}
